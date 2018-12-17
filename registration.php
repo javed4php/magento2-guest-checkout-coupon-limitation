3cf43f5e60e2d3d10c79d09074f72b61fa2b57d9
Javed <javed4php@gmail.com>
2018-12-17 17:49:16 +0530

    composer auto load

--- registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Javed_GuestCheckoutCouponLimitation',
-    __DIR__
-);
